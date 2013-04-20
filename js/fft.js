2efa3ec49ba7d9a6434ba5a3409b81f26bb65f17
Nemanja Miljković <proof1337@gmail.com>
2013-04-20 19:10:45 +0200

    Inverse FFT implementation.

--- js/fft.js
+++ js/fft.js
@@ -99,6 +99,69 @@
         internal_fft(x, 0, x.length, x.length);
     };
 
+    // inverse FFT using the Cooley-Tukey FFT in-place algorithm
+    // @param x      array    magnitude spectrum
+    // @param start  integer  start element index for this iteration
+    // @param N      integer  number of elements in this iteration
+    // @param FN     integer  number of elements in the x array
+    var internal_ifft = function(x, start, N, FN) {
+        if (N === 1)
+            return;
+
+        // we're constructing a butterfly
+        // if this section has N elements, we'll only go through the first N / 2
+        var limit = N / 2;
+
+        // step is equal to N / 2 and is used to skip N / 2 elements from our current element
+        var step = N / 2;
+
+        // self-explanatory
+        var end = start + limit;
+
+        // rotation factor 1 * exp(j 2 pi / FN)
+        var Wn = Complex.fromPolar(1, 2 * Math.PI / FN);
+
+        // currentPower is our rotation factor power
+        // starting from 0 and incrementing by FN / (2 * step) with each element
+        var currentPower = 0;
+
+        var oneHalf = Complex.from(0.5);
+
+        // iterate through each of the first N / 2 elements
+        for (var i = start; i < end; i++, currentPower++)
+        {
+            var x1 = x[i];
+            var x2 = x[i + step];
+
+            var old_x1 = x1.clone();
+            x1.add(x2).multiply(oneHalf);
+            x2.negate().add(old_x1);
+
+            // premultiply x2 with Wn^r
+            var currentWn = Wn.clone().pow(currentPower * FN / (2 * step));
+            x2.multiply(currentWn.multiply(oneHalf));
+        }
+
+        // recurse to calculate the left and right halves
+        internal_ifft(x, start, limit, FN);
+        internal_ifft(x, start + limit, limit, FN);
+    };
+
+    var ifft = function(x, N) {
+        if (N === undefined)
+            throw new Error("N is undefined!");
+
+        // resize the input array to N
+        while (x.length != N)
+            x.push(0);
+
+        for (var i = 0; i < x.length; i++)
+            x[i] = Complex.from(x[i]);
+
+        internal_ifft(x, 0, x.length, x.length);
+        bitwise_reorder(x);
+    };
+
     var magnitude = function(x) {
         var ret = [];
         for (var i = 0; i < x.length; i++)
@@ -109,6 +172,7 @@
 
 	window.log2 = log2;
     window.fft = fft;
+    window.ifft = ifft;
     window.magnitude = magnitude;
 
 })();
